128e685366998d3e4210781516f17fce453fca6a
sin.nguyen1 <sin.nguyen@hellohealthgroup.com>
2022-06-21 01:37:38 +0700

    push to HEAD

--- openvpn3_manager/disconnect_cli.sh
+++
@@ -1,3 +0,0 @@
-openvpn3 sessions-list | grep Path | awk -v OFS='\t' '{print $2}' | while read -r path; do
-    openvpn3 session-manage --path "$path" --disconnect
-done
--- openvpn3_manager/startsession.sh
+++
@@ -1,1 +0,0 @@
-openvpn3 session-start --config ./client.ovpn
